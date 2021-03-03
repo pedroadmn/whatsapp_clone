a74d90408d922993145c3bc39ded22d7c0dff2bb
Pedro Adelino de Mello Neto <pedro.mello@virtus.ufcg.edu.br>
2021-03-03 11:27:29 -0300

    Adding menu options and logout method

--- app/src/main/java/activities/MainActivity.java
+++ app/src/main/java/activities/MainActivity.java
@@ -1,23 +1,58 @@
 package activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import config.FirebaseConfig;
 import pedroadmn.whatsappclone.com.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firebaseAuth = FirebaseConfig.getFirebaseAuth();
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Whatsapp Clone");
         setSupportActionBar(toolbar);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logoutMenu:
+                logout();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        try {
+            firebaseAuth.signOut();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
