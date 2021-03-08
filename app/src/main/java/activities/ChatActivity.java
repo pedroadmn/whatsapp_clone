7a5d6d5414948c93f8f8c0ac786824c8e920159b
Pedro Adelino de Mello Neto <pedro.mello@virtus.ufcg.edu.br>
2021-03-08 16:42:01 -0300

    Sending imagem on chat

--- app/src/main/java/activities/ChatActivity.java
+++ app/src/main/java/activities/ChatActivity.java
@@ -1,5 +1,7 @@
 package activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,6 +11,8 @@ import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,12 +21,16 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.MediaStore;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import adapters.ChatAdapter;
 import config.FirebaseConfig;
@@ -37,6 +45,7 @@ public class ChatActivity extends AppCompatActivity {
 
     private TextView tvChatContactName;
     private CircleImageView civPhotoChat;
+    private ImageView ivMessagePhoto;
     private User recipientUser;
     private EditText etMessage;
     private FloatingActionButton fabSendMessage;
@@ -44,6 +53,7 @@ public class ChatActivity extends AppCompatActivity {
     private RecyclerView rvChatMessage;
     private ChatAdapter chatAdapter;
     private List<Message> messages = new ArrayList<>();
+    private static final int CAMERA_SELECTION = 100;
 
     private String senderUserId;
     private String recipientUserId;
@@ -51,6 +61,7 @@ public class ChatActivity extends AppCompatActivity {
     private DatabaseReference database;
     private DatabaseReference messageRef;
     private ChildEventListener childEventListenerMessages;
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +72,8 @@ public class ChatActivity extends AppCompatActivity {
         civPhotoChat = findViewById(R.id.civPhotoChat);
         etMessage = findViewById(R.id.etMessage);
 
+        ivMessagePhoto = findViewById(R.id.ivMessagePhoto);
+        ivMessagePhoto.setOnClickListener(v -> sendPhoto());
 
         fabSendMessage = findViewById(R.id.fabSendMessage);
         fabSendMessage.setOnClickListener(v -> sendMessage());
@@ -105,6 +118,8 @@ public class ChatActivity extends AppCompatActivity {
         rvChatMessage.setAdapter(chatAdapter);
 
         database = FirebaseConfig.getFirebaseDatabase();
+        storageRef = FirebaseConfig.getFirebaseStorage();
+
         messageRef = database.child("messages")
                 .child(senderUserId)
                 .child(recipientUserId);
@@ -182,5 +197,65 @@ public class ChatActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         messageRef.removeEventListener(childEventListenerMessages);
+    }
+
+    private void sendPhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, CAMERA_SELECTION);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Bitmap bitmap = null;
+
+            try {
+                switch (requestCode) {
+                    case CAMERA_SELECTION:
+                        bitmap = (Bitmap) data.getExtras().get("data");
+                        break;
+                }
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+
+            if (bitmap != null) {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+                byte[] imageData = baos.toByteArray();
+
+
+                String imageName = UUID.randomUUID().toString();
+
+                final StorageReference imageRef = storageRef
+                        .child("images")
+                        .child("photos")
+                        .child(senderUserId)
+                        .child(imageName + ".jpeg");
+
+                UploadTask uploadTask = imageRef.putBytes(imageData);
+                uploadTask
+                        .addOnFailureListener(e -> {
+                            Toast.makeText(ChatActivity.this, "Error on upload image", Toast.LENGTH_SHORT).show();
+                        })
+                        .addOnSuccessListener(taskSnapshot -> {
+                            Toast.makeText(ChatActivity.this, "Photo Successfully uploaded", Toast.LENGTH_SHORT).show();
+                            imageRef.getDownloadUrl().addOnCompleteListener(task -> {
+                                Uri url = task.getResult();
+                                Message message = new Message();
+                                message.setUserId(senderUserId);
+                                message.setMessage("image.jpeg");
+                                message.setImage(url.toString());
+
+                                saveMessage(senderUserId, recipientUserId, message);
+                                saveMessage(recipientUserId, senderUserId, message);
+                            });
+                        });
+            }
+        }
     }
 }
