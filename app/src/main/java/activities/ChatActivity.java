a482f613a7c9e21ba3b4cb7a7dda7aae054706ed
Pedro Adelino de Mello Neto <pedro.mello@virtus.ufcg.edu.br>
2021-03-08 14:19:15 -0300

    saving chat messages on firebase

--- app/src/main/java/activities/ChatActivity.java
+++ app/src/main/java/activities/ChatActivity.java
@@ -4,13 +4,22 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import config.FirebaseConfig;
 import de.hdodenhof.circleimageview.CircleImageView;
+import helper.Base64Custom;
+import helper.FirebaseUserHelper;
+import models.Message;
 import models.User;
 import pedroadmn.whatsappclone.com.R;
 
@@ -19,6 +28,12 @@ public class ChatActivity extends AppCompatActivity {
     private TextView tvChatContactName;
     private CircleImageView civPhotoChat;
     private User recipientUser;
+    private EditText etMessage;
+    private FloatingActionButton fabSendMessage;
+    private RecyclerView rvChatMessage;
+
+    private String senderUserId;
+    private String recipientUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +42,15 @@ public class ChatActivity extends AppCompatActivity {
 
         tvChatContactName = findViewById(R.id.tvChatContactName);
         civPhotoChat = findViewById(R.id.civPhotoChat);
+        etMessage = findViewById(R.id.etMessage);
+
+
+        fabSendMessage = findViewById(R.id.fabSendMessage);
+        fabSendMessage.setOnClickListener(v -> sendMessage());
+
+        rvChatMessage = findViewById(R.id.rvChatMessages);
+
+        senderUserId = FirebaseUserHelper.getUserId();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("");
@@ -52,6 +76,35 @@ public class ChatActivity extends AppCompatActivity {
             } else {
                 civPhotoChat.setImageResource(R.drawable.padrao);
             }
+
+            recipientUserId = Base64Custom.encodeBase64(recipientUser.getEmail());
         }
+    }
+
+    private void sendMessage() {
+        String textMessage = etMessage.getText().toString();
+
+        if (!textMessage.isEmpty()) {
+            Message message = new Message();
+            message.setUserId(senderUserId);
+            message.setMessage(textMessage);
+
+            saveMessage(senderUserId, recipientUserId, message);
+        } else {
+            Toast.makeText(this, "Type a message to send.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void saveMessage(String senderId, String recipientId, Message message) {
+        DatabaseReference database = FirebaseConfig.getFirebaseDatabase();
+        DatabaseReference messageRef = database.child("messages");
+
+        messageRef
+                .child(senderId)
+                .child(recipientId)
+                .push()
+                .setValue(message);
+
+        etMessage.setText("");
     }
 }
---
+++ app/src/main/java/models/Message.java
@@ -0,0 +1,35 @@
+package models;
+
+public class Message {
+
+    private String userId;
+    private String message;
+    private String image;
+
+    public Message() {
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
