447150f4bd608932899b1bc943317091b1e78de5
Pedro Adelino de Mello Neto <pedro.mello@virtus.ufcg.edu.br>
2021-03-08 15:34:20 -0300

    listing chat messages

--- app/src/main/java/activities/ChatActivity.java
+++ app/src/main/java/activities/ChatActivity.java
@@ -5,16 +5,26 @@ import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import adapters.ChatAdapter;
 import config.FirebaseConfig;
 import de.hdodenhof.circleimageview.CircleImageView;
 import helper.Base64Custom;
@@ -30,10 +40,17 @@ public class ChatActivity extends AppCompatActivity {
     private User recipientUser;
     private EditText etMessage;
     private FloatingActionButton fabSendMessage;
+
     private RecyclerView rvChatMessage;
+    private ChatAdapter chatAdapter;
+    private List<Message> messages = new ArrayList<>();
 
     private String senderUserId;
     private String recipientUserId;
+
+    private DatabaseReference database;
+    private DatabaseReference messageRef;
+    private ChildEventListener childEventListenerMessages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,6 +96,18 @@ public class ChatActivity extends AppCompatActivity {
 
             recipientUserId = Base64Custom.encodeBase64(recipientUser.getEmail());
         }
+
+        chatAdapter = new ChatAdapter(this, messages);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rvChatMessage.setLayoutManager(layoutManager);
+        rvChatMessage.setHasFixedSize(true);
+        rvChatMessage.setAdapter(chatAdapter);
+
+        database = FirebaseConfig.getFirebaseDatabase();
+        messageRef = database.child("messages")
+                .child(senderUserId)
+                .child(recipientUserId);
     }
 
     private void sendMessage() {
@@ -106,5 +135,48 @@ public class ChatActivity extends AppCompatActivity {
                 .setValue(message);
 
         etMessage.setText("");
+    }
+
+    private void getMessages() {
+        childEventListenerMessages = messageRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Message message = snapshot.getValue(Message.class);
+                messages.add(message);
+                chatAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getMessages();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        messageRef.removeEventListener(childEventListenerMessages);
     }
 }
---
+++ app/src/main/java/adapters/ChatAdapter.java
@@ -0,0 +1,96 @@
+package adapters;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import helper.FirebaseUserHelper;
+import models.Message;
+import pedroadmn.whatsappclone.com.R;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> {
+
+    private List<Message> messageList;
+    private Context context;
+
+    private static final int SENDER = 0;
+    private static final int RECIPIENT = 1;
+
+    public ChatAdapter(Context context, List<Message> messageList) {
+        this.messageList = messageList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View item;
+
+        if (viewType == SENDER) {
+            item = LayoutInflater.from(parent.getContext()).inflate(R.layout.sender_message_item_adapter, parent, false);
+        } else {
+            item = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipient_message_item_adapter, parent, false);
+        }
+        return new ChatAdapter.MyViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Message message = messageList.get(position);
+        String msg = message.getMessage();
+        String image = message.getImage();
+
+        if (image != null) {
+            Uri url = Uri.parse(image);
+
+            Glide.with(context)
+                    .load(url)
+                    .into(holder.ivChatMessagePhoto);
+
+            holder.tvMessage.setVisibility(View.GONE);
+        } else {
+            holder.tvMessage.setText(msg);
+            holder.ivChatMessagePhoto.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Message message = messageList.get(position);
+
+        String userId = FirebaseUserHelper.getUserId();
+
+        if (userId.equals(message.getUserId())) {
+            return SENDER;
+        }
+
+        return RECIPIENT;
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView tvMessage;
+        ImageView ivChatMessagePhoto;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvMessage = itemView.findViewById(R.id.tvChatMessage);
+            ivChatMessagePhoto = itemView.findViewById(R.id.ivChatMessagePhoto);
+        }
+    }
+}
